d2d4b283ac776e54b681e72ab150433287ea9bdd
Yan <yanvancelis@gmail.com>
2020-09-22 12:30:04 -0300

    Cards de conteúdo

---
+++ front-end/src/components/Content/style.js
@@ -0,0 +1,17 @@
+import styled, {css} from 'styled-components'
+
+export const SuperCard = styled.div`
+    width:730px;
+    border-radius: 5px;
+    background: #fff;
+    margin-top:-40px;
+    transition:0.3s;
+    border: solid 1.5px #fff;
+    transition:0.3s;
+
+    :hover {
+        transition:0.3s;
+        border: solid 1.5px #6BCB88;
+        cursor: pointer;
+    }
+`
